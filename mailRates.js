b2a23f15fdc14d52638b17a697040c49707e48a4
joerockclimber <joerockclimber@gmail.com>
2018-11-14 17:45:32 -0700

    dumb

--- mailRates.js
+++ mailRates.js
@@ -10,7 +10,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/getRate', getRate);
+app.get('/getRate', function(request, response){
+    getRate(request,response);
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
